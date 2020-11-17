668744f407e04b23a884d7706829572d3b8b9938
Bogdan-create <bogdan-shigaev@mail.ru>
2020-11-17 18:54:20 +0600

    fixed accordion

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 // partners
+let itemChild = document.querySelectorAll('.question__item__info')
+let itemParent = document.querySelectorAll('.question__item')
 
-document.querySelectorAll('.question__item').forEach((item) => 
-    item.addEventListener('click', () => {
-        if (item.classList.contains('open')) {
+itemParent.forEach((li,i) => {
+    li.addEventListener('click', (e) =>{
+        itemParent.forEach(item =>{
             item.classList.remove('open')
-        } else{
-                document
-                .querySelectorAll('.question__item')
-                .forEach((child) => child.classList.remove('open'))
-            item.classList.add('open')
-        }
+            item.style.paddingBottom = '16px'
+        })
+        let pHeight = itemChild[i].clientHeight
+        li.style.paddingBottom = `calc(10px + ${pHeight}px)`
+        e.target.classList.add('open')
     })
-);
+})
 
 //==========================================================================================
