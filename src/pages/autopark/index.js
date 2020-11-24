51a0db2324f6e6b9fb27af4929cbde630319ee01
Bogdan-create <bogdan-shigaev@mail.ru>
2020-11-24 21:59:27 +0600

    add burger

--- src/pages/autopark/index.js
+++ src/pages/autopark/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Hero from './hero';
 import Form from './form';
 
-function Autopark() {
+const Autopark = () => {
     return (
         <div className="autopark">
             <Hero />
             <Form />
         </div>
-    )
+    );
 }
 
 export default Autopark;
---
+++ src/pages/componets/header/const.js
@@ -0,0 +1,14 @@
+export const headerObj = [
+    {
+        link: '/',
+        text: 'Пользователям',
+    },
+    {
+        link: '/partners',
+        text: 'Водителям',
+    },
+    {
+        link: '/autopark',
+        text: 'Таксопаркам',
+    },
+];
